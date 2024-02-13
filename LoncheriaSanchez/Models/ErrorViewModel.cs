9ca0444eb8f49a02a9279b88f65703979a6e002f
JesúsEduardoCervantesSánchez <cervantessanchezjesuseduardo@gmail.com>
2024-02-13 14:14:06 -0600

    Se creo el proyecto

---
+++ LoncheriaSanchez/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoncheriaSanchez.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
